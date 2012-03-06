3c26053e21b67bda514017097fb07064055a0bad
The_Fly <richardassar@googlemail.com>
2012-03-06 04:07:18 +0000

    Initial commit

---
+++ worker.js
@@ -0,0 +1,51 @@
+importScripts('pngcrush.js');
+
+var line = '';
+
+Module.preRun = function() {
+        FS.init(function() {
+                return null;
+        }, function(data) {
+		if(data == 10) {
+			postMessage({
+				'type' : 'stdout',
+				'line' : line
+			});
+
+			line = '';
+		} else {
+			line += String.fromCharCode(data);
+		}
+        });
+};
+
+
+function getFileData(fileName) {
+	var data = FS.root.contents[fileName].contents;
+
+        return new Uint8Array(data).buffer;
+};
+
+
+onmessage = function(event) {
+	var message = event.data;
+
+	switch(message.type) {
+		case "file":
+			postMessage("FILE");
+			FS.createDataFile('/', 'input.png', message.data, true, false);
+			break;
+
+		case "command":
+			if(message.command == "go") {				
+				Module.run(['-reduce', 'input.png', 'output.png']);			
+
+				postMessage({
+					'type' : 'done',
+					'data' : getFileData('output.png')
+				});
+			}
+
+			break;
+	};
+};
